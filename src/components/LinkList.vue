107ed258986b26682aca891fc90339b94427ea05
Evan Ruby <evanruby94@gmail.com>
2017-09-26 18:26:37 -0400

    + Subscriptions - had to change version of subscriptions-transport-ws

--- src/components/LinkList.vue
+++ src/components/LinkList.vue
@@ -9,7 +9,7 @@
 
 <script>
 // 2 import the query as created
-import { ALL_LINKS_QUERY } from '../constants/graphql'
+import { ALL_LINKS_QUERY, NEW_LINKS_SUBSCRIPTION } from '../constants/graphql'
 import LinkItem from './LinkItem'
 
 export default {
@@ -27,7 +27,25 @@ export default {
   // 4 Add an apollo object to your component and add an allLinks property to it
   apollo: {
     allLinks: {
-      query: ALL_LINKS_QUERY
+      query: ALL_LINKS_QUERY,
+      subscribeToMore: [
+        // Each object in this array contains a document prop, which represents the subscription itself. In this case, it will fire for created events on the link type, i.e. every time a new link is created
+        {
+          document: NEW_LINKS_SUBSCRIPTION,
+          // The other propery here is updateQuery: similar to udpate, this function allows you to determine how the store should be udpated with the information sent by the server
+          updateQuery: (previous, { subscriptionData }) => {
+            const newAllLinks = [
+              subscriptionData.data.Link.node,
+              ...previous.allLinks
+            ]
+            const result = {
+              ...previous,
+              allLinks: newAllLinks.slice(0, 5)
+            }
+            return result
+          }
+        }
+      ]
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import App from './App'
 import router from './router'
 
 import { GC_USER_ID, GC_AUTH_TOKEN } from './constants/settings'
+import {
+  SubscriptionClient,
+  addGraphQLSubscriptions
+} from 'subscriptions-transport-ws'
 
 Vue.config.productionTip = false
 
@@ -16,6 +20,21 @@ Vue.config.productionTip = false
 const networkInterface = createBatchingNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj80yaozs09yp0125x224lo1x'
 })
+
+const wsClient = new SubscriptionClient(
+  'wss://subscriptions.us-west-2.graph.cool/v1/cj80yaozs09yp0125x224lo1x',
+  {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN)
+    }
+  }
+)
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
 
 networkInterface.use([
   {
@@ -29,10 +48,9 @@ networkInterface.use([
     }
   }
 ])
-
 // 4 instantiate the ApolloClient by passing in the networkInterface
 const apolloClient = new ApolloClient({
-  networkInterface,
+  networkInterface: networkInterfaceWithSubscriptions,
   connectToDevTools: true
 })
 
